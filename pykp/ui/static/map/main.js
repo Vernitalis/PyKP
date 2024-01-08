4992473d702d903e3712461ffb7aa024640317fc
Jan Kot <janymalino@gmail.com>
2024-01-08 12:51:34 +0100

    add OpenLayers map

---
+++ pykp/ui/static/map/main.js
@@ -0,0 +1,12 @@
+new ol.Map({
+  target: 'map-container',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM(),
+    }),
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([0, 0]),
+    zoom: 2,
+  }),
+});
