246e54b1100e0efdcbd13bff80c1114d6a71748c
Jan Kot <janymalino@gmail.com>
2024-01-08 20:41:54 +0100

    add stations points to the map

--- pykp/ui/static/map/main.js
+++ pykp/ui/static/map/main.js
@@ -34,9 +34,19 @@ const countryOutlineLayer = new ol.layer.Vector({
 	style: countryOutlineStyle
 })
 
+const stationsPointsLayer = new ol.layer.Vector({
+	source: new ol.source.Cluster({
+  		source: new ol.source.Vector({
+			format: new ol.format.GeoJSON,
+			url: "/geo/GetLocations"
+		})
+	})
+})
+
+
 const homeMap = new ol.Map({
 	target: 'map-container',
-	layers: [osmLayer, railwayLayer, countryOutlineLayer],
+	layers: [osmLayer, railwayLayer, countryOutlineLayer, stationsPointsLayer],
 	controls: [zoomControl],
 	view: new ol.View({
 		center: ol.proj.fromLonLat([19, 52]),
