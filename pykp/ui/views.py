bcba459e56d25f9e4207fffc12a8eb75b0fcfe5e
Jan Kot <janymalino@gmail.com>
2024-01-08 11:22:07 +0100

    remove folium rendering

--- pykp/ui/views.py
+++ pykp/ui/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpRequest
-from pykp.shared.models import Location
-import folium
-from folium.plugins import FastMarkerCluster
+# from pykp.shared.models import Location
 
 
 def homepage(request: HttpRequest):
-    m = folium.Map(
-        location=[52.0, 19.0],
-        zoom_start=7,
-        tiles="https://{s}.tiles.openrailwaymap.org/standard/{z}/{x}/{y}.png",
-        attr="Map data: {attribution.OpenStreetMap} | Map style: &copy;"
-        ' <a href="https://www.OpenRailwayMap.org">OpenRailwayMap</a> '
-        '(<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
-    )
-    locations = Location.objects.all()
-    markers = [(location.lat, location.lon) for location in locations]
-    cluster = FastMarkerCluster(data=markers)
-    cluster.add_to(m)
-
-    context = {"map": m._repr_html_()}
+    # locations = Location.objects.all()
+    context = {}
     return render(request, "index.html", context)
