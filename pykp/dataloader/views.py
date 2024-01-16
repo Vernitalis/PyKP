cb0999f439d496d6ae4baab5c113e52fd6f8e578
Jan Kot <janymalino@gmail.com>
2024-01-16 17:34:13 +0100

    refine the dataloader

--- pykp/dataloader/views.py
+++ pykp/dataloader/views.py
@@ -1,10 +1,11 @@
 import json
-from django.http import HttpRequest, HttpResponseBadRequest, HttpResponse
+from django.http import HttpRequest, JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 from pykp.shared.models import Station
+import logging
 
-# TODO: Add some more error logging
+logger = logging.getLogger()
 
 
 @csrf_exempt
@@ -14,21 +15,11 @@ def add_station(request: HttpRequest):
         body = request.body.decode()
         data = json.loads(body)
         Station.from_dict(data).save()
-        return HttpResponse(b"OK")
-    except Exception:
-        return HttpResponseBadRequest(b"Couldn't load the JSON file")
-
-
-@csrf_exempt
-@require_http_methods(["POST"])
-def import_stations(request: HttpRequest):
-    try:
-        body = request.body.decode()
-        data = json.loads(body)
-        print(data)
-        stations_list = [Station.from_dict(s) for s in data]
-        Station.objects.bulk_create(stations_list)
-        return HttpResponse(b"OK")
+        return JsonResponse({"status": "SUCCESS"})
     except Exception as e:
-        print(e)
-        return HttpResponseBadRequest(b"Couldn't load the JSON file")
+        logger.error("{type}: {message}".format(type=type(e), message=e))
+        response = JsonResponse(
+            {"status": "ERROR", "message": "Couldn't load the JSON file"}
+        )
+        response.status_code = 400
+        return response
