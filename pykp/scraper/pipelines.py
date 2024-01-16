aeb64f732b0cccb52b41157b14813cf191e6e43d
Jan Kot <janymalino@gmail.com>
2024-01-16 18:17:33 +0100

    add the HttpUploadItems pipeline

--- pykp/scraper/pipelines.py
+++ pykp/scraper/pipelines.py
@@ -1,4 +1,7 @@
 from itemadapter.adapter import ItemAdapter
+from scrapy.utils.defer import maybe_deferred_to_future
+from scrapy.http.request.json_request import JsonRequest
+from scrapy.exceptions import CloseSpider
 import json
 import os
 
@@ -16,3 +19,18 @@ class EnvironmentVariableOutput:
         variable_name = "PYKP_{name}_OUTPUT".format(name=spider.name.upper())
         output = json.dumps(self.items)
         os.environ[variable_name] = output
+
+
+class HttpUploadItems:
+    async def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+        request = JsonRequest(spider.upload_url, data=adapter.asdict())
+        try:
+            response = await maybe_deferred_to_future(
+                spider.crawler.engine.download(request)
+            )
+            if response.status != 200:
+                raise Exception
+        except Exception:
+            raise CloseSpider("An error during uploading the items has occured")
+        return item
